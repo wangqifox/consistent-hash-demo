43694b07fcdcd52643c9df578731e9c2c7baf68c
wangqi <wangq2880@163.com>
2018-08-20 09:05:29 +0800

    add comment

--- src/main/java/love/wangqi/Hash.java
+++ src/main/java/love/wangqi/Hash.java
@@ -6,9 +6,21 @@ package love.wangqi;
  * @date: Created in 2018/8/20 上午8:51
  */
 public interface Hash {
+    /**
+     * 初始化节点
+     * @return
+     */
     Hash initNodes();
 
+    /**
+     * 增加数据
+     * @param data
+     * @return
+     */
     int add(String data);
 
+    /**
+     * 统计信息
+     */
     void statistics();
 }
